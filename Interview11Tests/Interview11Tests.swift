1a640b4c14cf178d34cf8982b41606de061ee357
iamkane22 <manaflikenan222@gmail.com>
2024-11-02 14:01:56 +0400

    Initial Commit

---
+++ Interview11Tests/Interview11Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Interview11Tests.swift
+//  Interview11Tests
+//
+//  Created by Kenan on 02.11.24.
+//
+
+import Testing
+@testable import Interview11
+
+struct Interview11Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
